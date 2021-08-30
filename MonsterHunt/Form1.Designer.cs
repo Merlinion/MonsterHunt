a93867b75f33acd13aee56f6b66142b08f2103a2
Merlinion <60068989+Merlinion@users.noreply.github.com>
2021-08-30 20:03:28 +0200

    Add files via upload

---
+++ MonsterHunt/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace MonsterHunt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(81, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monster Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(77, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monster Race";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(81, 261);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Monster Rank";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(81, 356);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Monster Level";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(193, 68);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(193, 166);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(92, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(193, 261);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(92, 21);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 349);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(564, 296);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(564, 342);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(459, 299);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Gained Levels";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(470, 345);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Gained Gold";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Image = global::MonsterHunt.Properties.Resources.kill;
+            this.button1.Location = new System.Drawing.Point(439, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 35);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(320, 68);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(320, 265);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(320, 349);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(670, 299);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 19;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(670, 349);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 20;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::MonsterHunt.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(724, 447);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "MonsterHunt";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ MonsterHunt/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace MonsterHunt
+{
+
+    //Valamikezdete
+    public partial class Form1 : Form
+    {
+
+        public int[] ConvertType(int index)
+        {
+            
+            int[] temp = new int[2];
+            switch (index)
+            {
+                case 0:
+                    temp[0] = 3;
+                    temp[1] = 1;
+                    return temp;
+
+                case 1:
+                    temp[0] = 1;
+                    temp[1] = 3;
+                    return temp;
+                case 2:
+                    temp[0] = 2;
+                    temp[1] = 2;
+                    return temp;
+                default:
+                    temp[0] = 0;
+                    temp[1] = 0;
+                    return temp;
+            }
+        }
+
+        public int[] ConvertRace(int index)
+        {
+            int[] temp = new int[2];
+            switch (index)
+            {
+                case 0:
+                    temp[0] = 10;
+                    temp[1] = 12;
+                    return temp;
+
+                case 1:
+                    temp[0] = 12;
+                    temp[1] = 10;
+                    return temp;
+                case 2:
+                    temp[0] = 11;
+                    temp[1] = 11;
+                    return temp;
+                default:
+                    temp[0] = 0;
+                    temp[1] = 0;
+                    return temp;
+            }
+        }
+        public double ConvertRank(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 1;
+                case 1:
+                    return 1.5;
+                case 2:
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer("water.wav");
+            player.Play();
+            string[] type = new string[] { "Dragon", "Ooze", "Monstrous" };//string tombbe letrehozom az opciokat
+            string[] race1 = new string[] { "Drake", "Wyvern", "Wyrm" };
+            string[] race2 = new string[] { "Slime", "Jelly", "Sludge" };
+            string[] race3 = new string[] { "Undead", "Construct", "Elemental" };
+            string[] rank = new string[] { "Minion", "Miniboss", "Boss" };
+            textBox1.Enabled = false; //ne lehessen bele irni
+            textBox2.Enabled = false;
+            for (int i = 0; i < type.Length; i++)//feltoltom az 1.combot a tombom hossza a max.i++ lepteti
+            {
+                comboBox1.Items.Add(type[i]);
+            }
+
+            comboBox1.SelectedIndexChanged += (s, e) => //feltoltom a 2.combot az 1.combon alapulva, Changed eventtel
+            {
+                comboBox2.Items.Clear();
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    comboBox2.Items.Clear();
+                    pictureBox1.Image = Image.FromFile("dragon.png");
+                    for (int i = 0; i < race1.Length; i++)
+                    {
+                        comboBox2.Items.Add(race1[i]);
+                    }
+                }
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    comboBox2.Items.Clear();
+                    pictureBox1.Image = Image.FromFile("ooze.png");
+                    for (int i = 0; i < race2.Length; i++)
+                    {
+                        comboBox2.Items.Add(race2[i]);
+                    }
+                }
+                if (comboBox1.SelectedIndex == 2)
+                {
+                    pictureBox1.Image = Image.FromFile("monstrous.png");
+                    for (int i = 0; i < race3.Length; i++)
+                    {
+                        comboBox2.Items.Add(race3[i]);
+                    }
+
+                }
+            };
+            for (int i = 0; i < rank.Length; i++) //feltoltom a 3.combot
+            {
+                comboBox3.Items.Add(rank[i]);
+            }
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList; //normalisan mukodjon
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            pictureBox4.Image = Image.FromFile("exp.png");
+            pictureBox5.Image = Image.FromFile("gold.png");
+            
+
+
+
+            comboBox1.SelectedIndexChanged += (s, e) =>
+            {
+                System.Media.SoundPlayer player1 = new System.Media.SoundPlayer("lion.wav");
+                player1.Play();
+                switch (comboBox1.SelectedIndex) //kicsi ikon mert maskent nem megy
+                {
+                    case 0:
+                        {
+                            pictureBox1.Image = Image.FromFile("dragon.png");
+                            break;
+                        }
+                    case 1:
+                        {
+                            pictureBox1.Image = Image.FromFile("ooze.png");
+                            break;
+                        }
+                    case 2:
+                        {
+                            pictureBox1.Image = Image.FromFile("monstrous.png");
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            };
+ 
+            comboBox3.SelectedIndexChanged += (s, e) =>
+            {
+                System.Media.SoundPlayer player3 = new System.Media.SoundPlayer("dead.wav");
+                player3.Play();
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            pictureBox2.Image = Image.FromFile("minion.png");
+                            break;
+                        }
+                    case 1:
+                        {
+                            pictureBox2.Image = Image.FromFile("miniboss.png");
+                            break;
+                        }
+                    case 2:
+                        {
+                            pictureBox2.Image = Image.FromFile("boss.png");
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            };
+            numericUpDown1.ValueChanged += (s, e) =>
+            {
+                if (numericUpDown1.Value <= 35)
+                {
+                    pictureBox3.Image = Image.FromFile("op1.png");
+                }
+                if (numericUpDown1.Value > 35 && numericUpDown1.Value <= 75)
+                {
+                    pictureBox3.Image = Image.FromFile("op2.png");
+                }
+                if (numericUpDown1.Value > 75 && numericUpDown1.Value <= 100)
+                {
+                    pictureBox3.Image = Image.FromFile("op3.png");
+                }
+            };
+
+
+            
+            button1.TabStop = false;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+
+
+            button1.Click += (s, e) =>
+            {               
+               
+                textBox1.Text = Tapasztalat(comboBox1.SelectedIndex, comboBox2.SelectedIndex, comboBox3.SelectedIndex, Convert.ToInt32(numericUpDown1.Value)).ToString();
+                textBox2.Text = Arany(comboBox1.SelectedIndex, comboBox2.SelectedIndex, comboBox3.SelectedIndex, Convert.ToInt32(numericUpDown1.Value)).ToString();
+                if (Convert.ToInt32(textBox1.Text)> 0)
+                {
+                    MessageBox.Show("Ügyes vagy, " + textBox1.Text + " szintet léptél :)");
+                    System.Media.SoundPlayer player6 = new System.Media.SoundPlayer("yay.wav");
+                    player6.Play();
+
+                }
+                else
+                {
+                    MessageBox.Show("Sajnos meghaltál a küldetés közben :(");
+                    System.Media.SoundPlayer player5 = new System.Media.SoundPlayer("die.wav");
+                    player5.Play();
+                    
+                }
+               
+            };
+        }
+
+        public double ConvertSize(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 1.2;
+                case 1:
+                    return 2;
+                case 2:
+                    return 2.5;
+                default:
+                    return 0;
+            }
+        }
+
+
+        private double Arany(int combo1, int combo2, int combo3, int szint)
+        {
+            int[] tipi = ConvertType(combo1);
+            int[] raci = ConvertRace(combo2);
+
+            double gold = tipi[0] * raci[0] * (szint / 2) + ConvertRank(combo3);
+
+            return Math.Round(gold, 0);
+        }
+
+        private double Tapasztalat(int combo1, int combo2, int combo3, int szint)
+        {
+            int[] tipi = ConvertType(combo1);
+            int[] raci = ConvertRace(combo2);
+
+            double exp = (tipi[1] * raci[1] * (szint / 2) + ConvertRank(combo3))/10;
+
+            return Math.Round(exp, 0);
+        }
+
+    }
+
+}
